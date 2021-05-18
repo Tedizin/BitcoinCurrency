951375221fb084f47ac3f90453a722781fb801f0
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-05-18 17:31:31 -0300

    first commit

---
+++ BitcoinCurrency/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BitcoinCurrency
+//
+//  Created by Henrique Silva on 18/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    
+    
+    //MARK: - Variables
+    
+    var apiKey = "ZDMwYTMzN2Q2MmU4NDA4YWEwZDE0NDU1MTk1Y2Q5NjE"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
